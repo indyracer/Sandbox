1246a8c87c6bc2b1ecfc91037e1202580f10b3c6
Bob Cohen <bobcohen1969@yahoo.com>
2018-03-11 14:08:00 -0500

    Histogram printing

---
+++ src/chapter_7/Histogram.java
@@ -0,0 +1,35 @@
+package chapter_7;
+/*
+ * How to program Java
+ * Fig 7.7 Histogram Printing program
+ */
+
+import javax.swing.*;
+
+public class Histogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int array[] = {50, 3, 15, 7, 11, 9, 13, 5, 17, 1};
+		
+		String output = "Element\tValue\tHistogram";
+		
+		//for each elemen in array oupup a bar in histogram
+		for(int counter = 0; counter < array.length; counter++){
+			output += "\n" + counter + "\t" + array[counter] + "\t";
+			for(int stars = 0; stars <= array[counter]; stars++){
+				output += "*";
+			}
+		}
+		
+		JTextArea outputArea = new JTextArea();
+		outputArea.setText(output);
+		
+		JOptionPane.showMessageDialog(null, outputArea, "Histogram Printing Program", JOptionPane.INFORMATION_MESSAGE);
+		
+		System.exit(0);
+
+	}
+
+}
